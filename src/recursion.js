eaa653d938c774ec8d55832914e1525c64e94949
dprejs <danielnprejs@gmail.com>
2021-07-19 13:39:02 -0700

    createArray and reverseArr complete

--- src/recursion.js
+++ src/recursion.js
@@ -259,10 +259,18 @@ var compareStr = function(str1, str2) {
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
 var createArray = function(str) {
+  if (str.length === 1) {
+    return [str[0]];
+  }
+  return [str[0], createArray(str.slice(1))].flat(Infinity);
 };
 
 // 17. Reverse the order of an array
 var reverseArr = function(array) {
+  if (array.length === 1) {
+    return array[0];
+  }
+  return [array[array.length-1], reverseArr(array.slice(0,-1))].flat(Infinity);
 };
 
 // 18. Create a new array with a given value and length.
